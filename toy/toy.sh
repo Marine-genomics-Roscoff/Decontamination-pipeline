04165d49f4e965758b2f6a5b457533aae281adff
Fabio Rocha Jimenez Vieira <rocha@biologie.ens.fr>
2018-11-23 17:52:16 +0100

    Update toy.sh

--- toy/toy.sh
+++ toy/toy.sh
@@ -2,7 +2,8 @@
 gunzip toy/*.gz
 exit
 bash buildSpeciesTable.sh -i "toy/*.fa" > data/toy.speciesTable.tsv
-bash submitContamination_and_wait.sh -i "toy/*.fa"
+bash buildSpeciesTable.sh -i "toy_target/*.fa" >> data/toy.speciesTable.tsv
+bash submitContamination_and_wait.sh -i "toy/*.fa" -t "toy_target/*.fa"
 bash translateAndListCandidates.sh -i "results/*.candidates.tsv" -s data/toy.speciesTable.tsv -o toy
 bash exceptions.sh -i toy -s data/toy.speciesTable.tsv
 
