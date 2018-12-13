23943976e11c5344762e66439d4f0a4ab3969a3c
Fabio Rocha Jimenez Vieira <fabiorjvieira@gmail.com>
2018-12-13 15:33:25 +0100

    Update toy.sh

--- example/toy.sh
+++ example/toy.sh
@@ -5,13 +5,18 @@ exit
 
 bash buildSpeciesTable.sh -i "example/query/*.fa" > data/toy.speciesTable.tsv
 bash buildSpeciesTable.sh -i "example/ref/*.fa" >> data/toy.speciesTable.tsv
+exit
+
 #if you want to do all against all comparisons 
 bash submitContamination_and_wait.sh -q "example/query/*.fa"
 #else 
 bash submitContamination_and_wait.sh -q "example/query/*.fa" -r "example/ref/*.fa"
+exit
 
 echo "wait until the end of all jobs"
 exit
+
+echo "Fill the file data/knownExceptions.tsv with the exceptions with already know"
 
 bash translateAndListCandidates.sh -i "results/*.candidates.tsv" -s data/toy.speciesTable.tsv -o toy
 bash exceptions.sh -i toy -s data/toy.speciesTable.tsv
